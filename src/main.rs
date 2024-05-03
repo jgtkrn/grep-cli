68c78a0618b3062ae9ecec77126ac52039220376
jgtkrn <jagatkirana.jk@gmail.com>
2024-05-03 19:43:16 +0800

    GREP-0001: read and show content in file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect(); 
+    let query: &str = &args[1];
+    let path: &str = &args[2];
+
+    println!("searching for {}", query);
+    println!("in file{}", path);
+    let contents = fs::read_to_string(path).expect("should have to read now.");
+    println!("with contents: \r\n {}", contents);
+
 }
